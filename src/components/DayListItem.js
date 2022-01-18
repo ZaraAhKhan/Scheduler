132cfd6df2af7cd6491bd741170013685e066a42
ZaraAhKhan <88912965+NorwegianRidgeback@users.noreply.github.com>
2022-01-18 14:46:46 -0500

    Add day list item based on selected day

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 export default function DayListItem(props) {
-  const {name,spots} = props;
+  const {name,spots,setDay} = props;
 
-  return (
-    <li >
+  const handleClick = () => setDay(name);
+  
+    return (
+    <li onClick={handleClick}>
       <h2 className="text--regular">{name}</h2>
       <h3 className="text--light">{spots} spots remaining</h3>
     </li>
