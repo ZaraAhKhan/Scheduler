0d24f848c3e1988b422a4bed099c60500b4dc05e
ZaraAhKhan <88912965+NorwegianRidgeback@users.noreply.github.com>
2022-01-19 21:07:38 -0500

    Display Appointments

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
-  const { time } = props;
+  const { time, interview } = props;
   return (<article className="appointment">
-    {time?`Appointment at ${time}`:"No Appointments"}
+    <Header time={time} />
+    {interview? <Show student={interview.student} interviewer={interview.interviewer} /> : <Empty />}
   </article>);
 }
