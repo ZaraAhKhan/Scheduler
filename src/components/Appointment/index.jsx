048e68289dafed31042a5566c5c778d7949e0ba7
ZaraAhKhan <88912965+NorwegianRidgeback@users.noreply.github.com>
2022-01-22 12:50:35 -0500

    Conditionally Rendering Show and Empty using custom hook

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -3,11 +3,23 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
   const { time, interview } = props;
-  return (<article className="appointment">
-    <Header time={time} />
-    {interview? <Show student={interview.student} interviewer={interview.interviewer} /> : <Empty />}
-  </article>);
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
+  return (
+    <article className="appointment">
+      <Header time={time} />
+      {mode === EMPTY && <EMPTY onADD={() => console.log("Clicked onAdd")} />}
+      {mode === SHOW && (
+        <SHOW 
+        student={interview.student} 
+        interviewer={interview.interviewer}
+         />
+      )}
+    </article>
+  );
 }
