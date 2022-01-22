f70165eee67ce807e485ee79d98055db5a39a94f
ZaraAhKhan <88912965+NorwegianRidgeback@users.noreply.github.com>
2022-01-22 12:36:01 -0500

    Write custom hook for back and transititon functionalities

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,14 +2,23 @@ import { useState } from "react";
 
 export default function useVisualMode(initialMode) {
   const [mode,setMode] = useState(initialMode);
+  const [history,setHistory] = useState([initialMode]);
 
-  function transition(newMode) {
+  function transition(newMode,replace = false) {
     setMode(newMode);
+    if(replace){
+      history.pop();
+    }
+    setHistory(prev => ([...prev,newMode]));
   }
 
   function back() {
+    history.pop();
+    if(history.length) {
+      setMode(() => ( history[history.length-1]));
+    }
     
   }
 
-  return { mode,transition };
+  return { mode,transition,back };
 }
