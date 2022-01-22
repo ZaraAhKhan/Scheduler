3b445edc4bb5ee9f94f095f54b06c1d6515c925f
ZaraAhKhan <88912965+NorwegianRidgeback@users.noreply.github.com>
2022-01-22 13:23:54 -0500

    Add tests for getInterviewersForDay helper function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -18,6 +18,25 @@
   return result;
 }
 
+function getInterviewersForDay(state,day) {
+  let appointmentsForDay;
+  let result = [];
+  for(let dayOfTheWeek of state.days){
+    if(dayOfTheWeek.name === day){
+      appointmentsForDay = dayOfTheWeek.appointments;
+    }
+  }
+  if(!appointmentsForDay){
+    return [];
+  }
+  for(let appointmentObj of Object.values(state.appointments)){
+    if(appointmentsForDay.includes(appointmentObj.id)){
+      result.push(appointmentObj);
+    }
+  }
+  return result;
+}
+
 
 function getInterview(state,interview){
   if(!interview){
@@ -31,4 +50,4 @@ function getInterview(state,interview){
   return interview;
  }
 
-module.exports = {getAppointmentsForDay, getInterview}
+module.exports = {getAppointmentsForDay, getInterview, getInterviewersForDay}
