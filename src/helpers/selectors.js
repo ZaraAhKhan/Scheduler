129f385f75cc65304f52ea3ee3d7bc0e303a5163
ZaraAhKhan <88912965+NorwegianRidgeback@users.noreply.github.com>
2022-01-23 19:54:52 -0500

    Refactor helper functions

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,53 +1,39 @@
+function getAppointmentsForDay(state, day) {
+  let dayArray = state.days.filter((dayOfTheWeek) => dayOfTheWeek.name === day);
 
- function getAppointmentsForDay(state,day) {
-  let appointmentsForDay;
-  let result = [];
-  for(let dayOfTheWeek of state.days){
-    if(dayOfTheWeek.name === day){
-      appointmentsForDay = dayOfTheWeek.appointments;
-    }
+  if (dayArray.length === 0) {
+    return dayArray;
   }
-  if(!appointmentsForDay){
-    return [];
-  }
-  for(let appointmentObj of Object.values(state.appointments)){
-    if(appointmentsForDay.includes(appointmentObj.id)){
-      result.push(appointmentObj);
-    }
-  }
+
+  let result = Object.values(state.appointments).filter((appointmentObj) =>
+    dayArray[0].appointments.includes(appointmentObj.id)
+  );
   return result;
 }
 
-function getInterviewersForDay(state,day) {
-  let getInterviewersForDay;
-  let result = [];
-  for(let dayOfTheWeek of state.days){
-    if(dayOfTheWeek.name === day){
-      getInterviewersForDay = dayOfTheWeek.interviewers;
-    }
+function getInterviewersForDay(state, day) {
+  let dayArray = state.days.filter((dayOfTheWeek) => dayOfTheWeek.name === day);
+
+  if (dayArray.length === 0) {
+    return dayArray;
   }
-  if(!getInterviewersForDay){
-    return [];
-  }
-  for(let appointmentObj of Object.values(state.interviewers)){
-    if(getInterviewersForDay.includes(appointmentObj.id)){
-      result.push(appointmentObj);
-    }
-  }
+
+  let result = Object.values(state.interviewers).filter((appointmentObj) =>
+    dayArray[0].interviewers.includes(appointmentObj.id)
+  );
   return result;
 }
 
-
-function getInterview(state,interview){
-  if(!interview){
+function getInterview(state, interview) {
+  if (!interview) {
     return null;
   }
-  for(let interviewer of Object.values(state.interviewers)){
-    if(interviewer.id === interview.interviewer){
+  for (let interviewer of Object.values(state.interviewers)) {
+    if (interviewer.id === interview.interviewer) {
       interview.interviewer = interviewer;
     }
   }
   return interview;
- }
+}
 
-module.exports = {getAppointmentsForDay, getInterview, getInterviewersForDay}
+module.exports = { getAppointmentsForDay, getInterview, getInterviewersForDay };
